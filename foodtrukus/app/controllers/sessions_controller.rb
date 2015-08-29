a656b61ee4fd025542403593ca0028f0634af271
jabari-dawkins <jabaridawkins@gmail.com>
2015-08-29 11:41:58 -0400

    Edit user form (wip)

--- foodtrukus/app/controllers/sessions_controller.rb
+++ foodtrukus/app/controllers/sessions_controller.rb
@@ -4,9 +4,17 @@ class SessionsController < ApplicationController
   def create
     if user && user.authenticate(params[:password])
       sign_in(user)
-      redirect_back_or_to(:users)
-    else
+      redirect_back_or_to(:food_trucks)
+    elsif
       render :new
+    else
+      new_user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password])
+      if new_user.save
+        sign_in(new_user)
+        redirect_to food_trucks
+      else
+        render :new
+      end
     end
   end
 
